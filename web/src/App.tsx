ad5fd96c2c4daa739051bd88b9fffbcdddc13895
Kittichote Kamalapirat <shane@Kittichotes-MacBook-Air.local>
2022-01-25 11:48:57 +0700

    Ⓜ️ add material ui to web folder

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,17 +2,19 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { useMeQuery } from "./generated/graphql";
 import { Home } from "./Home";
+import { ThemeProvider } from "@material-ui/core";
+import { makeStyles } from "@mui/styles";
+import { theme } from "./theme";
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} /> {/* list all the restaurants */}
-        {/* <Route path="restaurants">
-          <Route path="res_id" element={<Restaurant />} /> 
-        </Route> */}
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 };
 
