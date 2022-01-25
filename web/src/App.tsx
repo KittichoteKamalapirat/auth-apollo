b88a9d0f51fdc776d0495c20725cc24c32caffaa
Kittichote Kamalapirat <shane@Kittichotes-MacBook-Air.local>
2022-01-25 10:44:47 +0700

    🛡️ add create react app withtypescript, add apollo, graphql codegen

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useMeQuery } from "./generated/graphql";
 
 function App() {
+  const { data, loading } = useMeQuery({});
+  console.log({ data });
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
